8126c8812fb5bd3df141ee5cd6e00ab10532db02
Игорь Сидоров <onixian90@gmail.com>
2021-10-14 15:57:50 +0300

    kek

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('Fight...');
+const player1 = {
+    name: player1,
+    hp: 100,
+    img:
+    };
+    
+    const player2 = {
+        
+    };
+    
+    
+    const $p1 = document.createElement('div');
+    $p1.innerText = 'Scorpion';
+    $p1.classList.add('p1');
+    
+    const $root = document.querySelector('.root');
+    
+    $root.appendChild($p1);
